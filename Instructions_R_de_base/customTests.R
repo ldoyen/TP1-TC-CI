10de8d849f88c3431b759a2d134fb4a4b07c91e5
ldoyen <laurent.doyen@univ-grenoble-alpes.fr>
2019-08-23 13:53:00 +0200

    test MAJ sauvegarde log

--- Instructions_R_de_base/customTests.R
+++ Instructions_R_de_base/customTests.R
@@ -71,22 +71,89 @@ if(selection %in% 1:5){
   names(log_tbl) <- c(num_etud, nom_etud, prenom_etud,log_$lesson_name,"t")
   write.csv(log_tbl, file = temp, row.names = FALSE)
   encoded_log <- base64encode(temp)
-  browseURL(paste0(pre_fill_link, encoded_log))
+  e <- get("e", parent.frame())
+  e$url_googleForm<-paste0(pre_fill_link, encoded_log)
+  #browseURL(paste0(pre_fill_link, encoded_log)
+  readline("Swirl va maintenant ouvrir un Google Form dans votre navigateur web. Tapez sur la touche Entrée.")
+  browseURL(e$url_googleForm)
 
   e <- get("e", parent.frame())
-  if(selection %in% c(1,2,3)) e$adresse_email<-"laurent.doyen@iut2.univ-grenoble-alpes.fr" else e$adresse_email<-"marie-jose.martinez@iut2.univ-grenoble-alpes.fr"
-  e$sujet_email<-paste0("**TP1-TC-CI**"," G",selection,", ",log_$lesson_name,", ", nom_etud,collapse="")
-  e$corp_email<-encoded_log
-}
+    if(selection %in% c(1,2,3)) e$adresse_email<-"laurent.doyen@iut2.univ-grenoble-alpes.fr" else e$adresse_email<-"marie-jose.martinez@iut2.univ-grenoble-alpes.fr"
+    e$sujet_email<-paste0("**TP3-TC-CI**"," G",selection,", ",log_$lesson_name,", ", nom_etud,collapse="")
+    e$corp_email<-encoded_log
+  }
   return(res)
 }
+
+googleForm_log<-function(){
+  e <- get("e", parent.frame())
+  if(regexpr("Google Form",e$val)!=-1){
+    res<-FALSE
+    browseURL(e$url_googleForm)
+  } else {
+    res<-TRUE
+   readline("Swirl va maintenant ouvrir un email dans votre logiciel de messagerie. Tapez sur la touche Entrée.")
+    email(e$adresse_email,e$sujet_email,e$corp_email)
+  }
+  return(res)
+}
+
 
 email_log<-function(){
   e <- get("e", parent.frame())
-  res<-FALSE
-  if(e$val=="Oui"){
+  res<-TRUE
+  if(regexpr("email",e$val)!=-1){
+    res<-FALSE
     email(e$adresse_email,e$sujet_email,e$corp_email)
-    res<-TRUE
   }
   return(res)
+}
+
+sauve_log<-function(){
+  demande<-"Appuyez sur Entr\xE9, puis choississez un r\xE9pertoire dans lequel sauver votre cl\xE9. Attention, dans les salles machine de l'IUT, choississez un r\xE9pertoire personnel."
+  Encoding(demande) <- "latin1"
+  rep <- readline(demande)
+  path <- choose_dir()
+  if(length(path)==0){
+    return(FALSE)
+  } else {
+    setwd(path)
+    e <- get("e", parent.frame())
+    encoded_log<-e$encoded_log
+    log_tbl<-e$log_tbl
+    log_ <- getLog()
+    e$fichier<-paste0("TP2",log_$lesson_name,".R")
+
+    save(log_tbl,encoded_log,file=e$fichier)
+    demande<-paste0("Votre cl\xE9, est sauv\xE9 dans le fichier ",e$fichier," Tapez sur la touche Entr\xE9e pour continuer.")
+    Encoding(demande) <- "latin1"
+    rep <- readline(demande)
+    return(TRUE)
+  }
+}
+
+qsauve_log<-function(){
+e <- get("e", parent.frame())
+if(e$val=="Oui"){
+  return(TRUE)
+} else {
+  demande<-"Appuyez sur Entr\xE9, puis choississez un r\xE9pertoire dans lequel sauver votre cl\xE9. Attention, dans les salles machine de l'IUT, choississez un r\xE9pertoire personnel."
+  Encoding(demande) <- "latin1"
+  rep <- readline(demande)
+  path <- choose_dir()
+  if(length(path)==0){
+    return(FALSE)
+  } else {
+    setwd(path)
+    e <- get("e", parent.frame())
+    encoded_log<-e$encoded_log
+    log_tbl<-e$log_tbl
+
+    save(log_tbl,encoded_log,file=e$fichier)
+    demande<-paste0("Votre cl\xE9, est sauv\xE9 dans le fichier ",e$fichier," Tapez sur la touche Entr\xE9e pour continuer.")
+    Encoding(demande) <- "latin1"
+    rep <- readline(demande)
+    return(FALSE)
+  }
+}
 }
